88b19d484c03021bc8bbd20c06fd06a7f9dc0c34
jacobbiannz <jacob.bian@gmail.com>
2016-08-13 22:13:14 +1200

    Add project files.

---
+++ SNOW_Solution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SNOW_Solution.MvcApplication" Language="C#" %>
---
+++ SNOW_Solution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SNOW_Solution.Startup))]
+namespace SNOW_Solution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
