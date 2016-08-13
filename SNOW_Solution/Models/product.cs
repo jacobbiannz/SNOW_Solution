c08df0d0c52aa0e87f66ce87806106b79985047a
jacobbiannz <jacob.bian@gmail.com>
2016-08-13 22:31:39 +1200

    commit

---
+++ SNOW_Solution/Models/product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SNOW_Solution.Models
+{
+    public class product
+    {
+        public int ProductId { get; set; }
+    }
+}
