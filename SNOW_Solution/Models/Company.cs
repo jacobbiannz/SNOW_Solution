c44b38ee6fa38edad1ead4e1976a2387f416b2c8
jacobbiannz <jacob.bian@gmail.com>
2016-08-14 13:31:28 +1200

    new classes

---
+++ SNOW_Solution/Models/Company.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SNOW_Solution.Models
+{
+    public class Company : Contact
+    {
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Store> AllStores { get; set; }
+
+        public List<Role> AllRoles { get; set; }
+
+        public List<Permission> AllPermissions { get; set; }
+
+        public List<AdminUser> AllUsers { get; set; }
+
+        public List<product> AllProducts { get; set; }
+
+        public List<Customer> AllCustomers { get; set; }
+
+        public List<KeyWord> AllKeyWords { get; set; }
+
+        public List<Promotion> AllPromotions { get; set; }
+
+        public List<Brand> AllBrands { get; set; }
+
+        public List<Category> AllCategories{ get; set; }
+
+        public List<OrderStatus> AllOrderStatus { get; set; }
+
+        public List<PaymentType> AllPaymentTypes { get; set; }
+
+        public List<SizeType> AllSizeTypes { get; set; }
+    }
+}
