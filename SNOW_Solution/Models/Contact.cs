81cfa0cbf2b46c1dc5bc6c7134646c702993c75a
jacobbiannz <jacob.bian@gmail.com>
2016-08-14 12:44:34 +1200

    Contact

---
+++ SNOW_Solution/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SNOW_Solution.Models
+{
+    public abstract class Contact
+    {
+        public string NameFirst{ get; set; }
+
+        public string NameLast { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string Password { get; set; }
+
+        //public Country MyCountry { get; set; }
+
+        //public City Mycity { get; set; }
+    }
+}
