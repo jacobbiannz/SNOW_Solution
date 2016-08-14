282886e5ea8db8e1f32aa490232be797b69830c6
jacobbiannz <jacob.bian@gmail.com>
2016-08-14 13:41:35 +1200

    comment

--- SNOW_Solution/Models/Company.cs
+++ SNOW_Solution/Models/Company.cs
@@ -11,30 +11,30 @@ namespace SNOW_Solution.Models
 
         public string Name { get; set; }
 
-        public List<Store> AllStores { get; set; }
+        //public List<Store> AllStores { get; set; }
 
-        public List<Role> AllRoles { get; set; }
+        //public List<Role> AllRoles { get; set; }
 
-        public List<Permission> AllPermissions { get; set; }
+        //public List<Permission> AllPermissions { get; set; }
 
         public List<AdminUser> AllUsers { get; set; }
 
-        public List<product> AllProducts { get; set; }
+        //public List<product> AllProducts { get; set; }
 
         public List<Customer> AllCustomers { get; set; }
 
-        public List<KeyWord> AllKeyWords { get; set; }
+        //public List<KeyWord> AllKeyWords { get; set; }
 
-        public List<Promotion> AllPromotions { get; set; }
+       // public List<Promotion> AllPromotions { get; set; }
 
-        public List<Brand> AllBrands { get; set; }
+        //public List<Brand> AllBrands { get; set; }
 
-        public List<Category> AllCategories{ get; set; }
+        //public List<Category> AllCategories{ get; set; }
 
-        public List<OrderStatus> AllOrderStatus { get; set; }
+        //public List<OrderStatus> AllOrderStatus { get; set; }
 
-        public List<PaymentType> AllPaymentTypes { get; set; }
+        //public List<PaymentType> AllPaymentTypes { get; set; }
 
-        public List<SizeType> AllSizeTypes { get; set; }
+        //public List<SizeType> AllSizeTypes { get; set; }
     }
 }
