3c7e79434a0b20af197c003185c9f00437001112
boChuck <bo.rubyxing@gmail.com>
2016-09-30 01:08:51 +1300

    generic repository added

---
+++ NUnit.Tests/GenericRepositoryTest/TestGenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using SNOW_Solution.Controllers;
+using SNOW_Solution.Models;
+using SNOW_Solution.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NUnit.Tests.GenericRepositoryTest
+{
+    public class TestGenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> SelectAll()
+        {
+            return null;
+    }
+
+        public T SelectByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SNOW_Solution/Repository/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using SNOW_Solution.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SNOW_Solution.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private CompanyDbContext db = null;
+        private DbSet<T> table = null;
+
+        public GenericRepository()
+        {
+            this.db = new CompanyDbContext();
+            table = db.Set<T>();
+        }
+
+        public GenericRepository(CompanyDbContext db)
+        {
+            this.db = db;
+            table = db.Set<T>();
+        }
+
+        public IEnumerable<T> SelectAll()
+        {
+            return table.ToList();
+        }
+
+        public T SelectByID(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            db.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
