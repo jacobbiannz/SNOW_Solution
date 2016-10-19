e960ca0617121e0ecec91ed21a39b8333de11f5e
boChuck <bo.rubyxing@gmail.com>
2016-10-20 00:14:59 +1300

    unity config

---
+++ SNOW_Solution/App_Start/UnityConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SNOW_Solution.App_Start
+{
+    public class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            
+           
+        }
+    }
+}
